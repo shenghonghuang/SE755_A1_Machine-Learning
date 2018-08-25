a3f3704916ce14f3caf08f6eb95b667a0ca1bc5a
Shenghong Huang <shenghong.huang@hotmail.com>
2018-08-26 11:55:30 +1200

    Complete occupancy classification

--- Occupancy_sensor/Classification.py
+++ Occupancy_sensor/Classification.py
@@ -6,25 +6,54 @@ Created on Sat Aug 11 13:27:20 2018
 """
 
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import Imputer
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-import category_encoders as cs
-from sklearn.pipeline import FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+data=pd.read_csv('occupancy_sensor_data.csv')
+print(data.head(5))
+columns=data.columns
+print(columns)
 
-occupancy=pd.read_csv(r'D:\Projects\755\SE755_A1_Machine-Learning\Occupancy_sensor\occupancy_sensor_data.csv',index_col=0)
-occupancy.describe()
 
-#attributes
-o_features=occupancy.iloc[:,0:5].copy()
-o_target=occupancy.iloc[:,5].copy()
+features=['Temperature', 'Humidity', 'Light', 'CO2', 'HumidityRatio']
+target=['Occupancy']
+x=data[features].values
+y=data[target].values
+x_train, x_test,y_train,y_test= train_test_split(x,y,test_size=0.2,random_state=1)
 
-for i in range(0,len(x)):
-    for j in range(0,len(x[i][j])):
-        thisdata=x[i][j]
-        if (thisdata==0):
-            x[i][j]=int(1)
-        else:
-            x[i][j]=int(0)
+#===================Perceptron=========================
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+y_pred = ppn.fit(x_train, y_train).predict(x_test)
+print('Perceptron:')
+print(classification_report(y_test,y_pred))
+
+#===================SVM=========================
+from sklearn import svm,datasets
+#调用SVC()
+clf = svm.SVC()
+y_pred = clf.fit(x_train, y_train).predict(x_test)
+print('SVM:')
+print(classification_report(y_test,y_pred))
+
+#===================Decision Trees=========================
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+y_pred = clf.fit(x_train, y_train).predict(x_test)
+print('Decision trees:')
+print(classification_report(y_test,y_pred))
+
+#=================== Nearest neighbour classifier=========================
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier()
+y_pred = clf.fit(x_train, y_train).predict(x_test)
+print('Nearest neighbour classifier:')
+print(classification_report(y_test,y_pred))
+
+#===================  Naïve Bayes classifier=========================
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+y_pred = clf.fit(x_train, y_train).predict(x_test)
+print('Naive Bayes classifier:')
+print(classification_report(y_test,y_pred))
+
+
