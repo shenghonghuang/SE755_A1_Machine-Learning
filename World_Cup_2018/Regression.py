cfc98ef8837b9c8cc0150f219092450bc57015ec
Shenghong Huang <shenghong.huang@hotmail.com>
2018-08-30 17:37:38 +1200

    Update comments

--- World_Cup_2018/Regression.py
+++ World_Cup_2018/Regression.py
@@ -14,31 +14,29 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 pd.set_option('max_colwidth',200)
 
-#1 首先获取整个表的内容，去掉一列不需要的
+# Preprocessing dataset
 worldcup=pd.read_csv('2018 worldcup.csv',index_col=0)
-#match date is assumed to be irrelevant for the match results
+# Drop irrelevant column
 worldcup.drop(['Date'],axis=1,inplace=True)
 worldcup.describe()
 
-#将文字转换成数值型的
+# Convert words to number
 
-#先统计一下位置信息
+# Calculate the number of locations
 Location=worldcup['Location'].unique()
 print('Location={0}'.format(len(Location)))
 #
 Phase=worldcup['Phase'].unique()
 print('Phase={0}'.format(len(Phase)))
 
-# drop掉不相关信息
+# Drop irrelevant columns for features
 worldcup=worldcup.drop(['Location','Normal_Time'],axis=1)
 print(worldcup.shape)
 
-#继续筛选特征，去掉['Team1','Team1_Continent','Team2','Team2_Continent']
+# Drop irrelevant columns for features
 worldcup=worldcup.drop(['Team1','Team1_Continent','Team2','Team2_Continent'],axis=1)
 
-#以上是我们抽取出来的通用的特征
-#选取Score作为target,并忽略Win/loss/draw属性
-#用不到的Match_result，Team1_Pass_Accuracy(%)，Team2_Pass_Accuracy(%)，Team1_Ball_Possession(%)，Team2_Ball_Possession(%)
+# Drop irrelevant columns for features
 worldcup=worldcup.drop(['Match_result','Team1_Pass_Accuracy(%)','Team2_Pass_Accuracy(%)','Team1_Ball_Possession(%)','Team2_Ball_Possession(%)',],axis=1)
 
 print(worldcup.columns)
@@ -46,28 +44,32 @@ print(worldcup.columns)
 worldcup=pd.get_dummies(worldcup,prefix='Phase')
 print(worldcup.shape)
 
+# Selection useful columns for regression
 featrues=['Team1_Attempts', 'Team1_Corners', 'Team1_Distance_Covered',
           'Team2_Attempts', 'Team2_Corners', 'Team2_Distance_Covered']
 target=['Total_Scores']
 print(worldcup.head(5))
 x=worldcup[featrues].values
 y=worldcup[target].values
-#将数据分成训练集和测试集，用测试集评价模型
-x_train, x_test,y_train,y_test= train_test_split(x,y,test_size=0.2,random_state=1)
-#使用sklearn库构建模型
 
+# Split the dataset into training dataset and testing dataset
+x_train, x_test,y_train,y_test= train_test_split(x,y,test_size=0.2,random_state=1)
+
+# Use SK-Learn generate models
 #=======================Ordinary Regression============
 
 lr = linear_model.LinearRegression().fit(x_train,y_train)
 y_predict=lr.predict(x_test)
 print(lr.score(x_test, y_test))
-# print model
+# Print the model
 p1=plt.scatter(range(len(x_test)), y_test,  color='black')
-#用predict预测，这里预测输入x对应的值，进行画线
+
+# Prediction and draw the diagram
 p2=plt.plot(range(len(x_test)), y_predict, color='red', linewidth=1)
 plt.legend(["predict", "true"], loc='upper right')
 plt.title('Ordinary Regression')
 plt.show()
+
 # The mean squared error
 print('_________________###################____________________')
 print("Mean squared error for testing data: %.2f"
@@ -83,8 +85,9 @@ from sklearn.linear_model import Ridge
 clf = Ridge(alpha=.5)
 clf.fit(x_train,y_train)
 y_predict=clf.predict(x_test)
+# Print the model
 p1=plt.scatter(range(len(x_test)), y_test,  color='black')
-#用predict预测，这里预测输入x对应的值，进行画线
+# Prediction and draw the diagram
 p2=plt.plot(range(len(x_test)), y_predict, color='red', linewidth=1)
 plt.legend(["predict", "true"], loc='upper right')
 plt.title('Ridge Regression')
