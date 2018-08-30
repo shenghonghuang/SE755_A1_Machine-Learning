e45285857e18bae037942623458f5813acc7ff12
Shenghong Huang <shenghong.huang@hotmail.com>
2018-08-30 21:53:34 +1200

    Change the features for traffic regression

--- Traffic_flow/Regression.py
+++ Traffic_flow/Regression.py
@@ -14,12 +14,14 @@ from sklearn.metrics import mean_squared_error, r2_score
 traffic=pd.read_csv('traffic_flow_data.csv')
 print(traffic.head(5))
 columns=traffic.columns
-features=columns[0:-1]#除去最后一列，前面的做特征
-target=features[-1]
+#features=columns[0:-1]#除去最后一列，前面的做特征
+features=['Segment_45(t)']
+#target=features[-1]
+target=['Segment23_(t+1)']
 
 x=traffic[features].values
 y=traffic[target].values
-#将数据分成训练集和测试集，用测试集评价模型
+# Split the dataset into training dataset and testing dataset
 x_train, x_test,y_train,y_test= train_test_split(x,y,test_size=0.2,random_state=1)
 #=======================Ordinary Regression============
 
@@ -28,7 +30,7 @@ y_predict=lr.predict(x_test)
 print(lr.score(x_test, y_test))
 # print model
 p1=plt.scatter(range(len(x_test)), y_test,  color='black')
-#用predict预测，这里预测输入x对应的值，进行画线
+# Prediction and draw the diagram
 p2=plt.plot(range(len(x_test)), y_predict, color='red', linewidth=1)
 plt.legend(["predict", "true"], loc='upper right')
 plt.title('Ordinary Regression')
@@ -50,7 +52,7 @@ clf.fit(x_train,y_train)
 y_predict=clf.predict(x_test)
 # print model
 p1=plt.scatter(range(len(x_test)), y_test,  color='black')
-#用predict预测，这里预测输入x对应的值，进行画线
+# Prediction and draw the diagram
 p2=plt.plot(range(len(x_test)), y_predict, color='red', linewidth=1)
 plt.legend(["predict", "true"], loc='upper right')
 plt.title('Ridge Regression')
